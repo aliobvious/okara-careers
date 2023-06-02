0011da4b6560812cbdbcae69ce833416c88c11e3
aliobvious <122002757+aliobvious@users.noreply.github.com>
2023-06-02 22:55:25 +0000

    dB-UPDATE

--- database.py
+++ database.py
@@ -1,21 +1,47 @@
-from sqlalchemy import create_engine,text
+from sqlalchemy import create_engine, text
+import os
 
-db_connection_string={  create_engine("mysql+pymysql://uk7bg6n6qjx5s9g379h5:pscale_pw_Ftzy7UhC3xGlM13jXrFR0pSOZJhXLcBK2S6wSEaFBx2@aws.connect.psdb.cloud/okaracareer?charset=utf8b4")
-             }
+db_connection_string = os.environ['DB_CONNECTION_STRING']
 
 engine = create_engine(
-  db_connection_string,
+  db_connection_string, 
   connect_args={
-    "ssl":{
-      "ssl_ca": "etc/ssl/cert.perm"
+    "ssl": {
+      "ssl_ca": "/etc/ssl/cert.pem"
     }
-  }
-)
+  })
 
 
-with engine.connect() as conn:
-  result = conn.execute(text("select * from jobs"))
-  print(result.all())
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    jobs = []
+    for row in result.all():
+      jobs.append(dict(row))
+    return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text("SELECT * FROM jobs WHERE id = :val"),
+      val=id
+    )
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0])
 
 
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
 
+    conn.execute(query, 
+                 job_id=job_id, 
+                 full_name=data['full_name'],
+                 email=data['email'],
+                 linkedin_url=data['linkedin_url'],
+                 education=data['education'],
+                 work_experience=data['work_experience'],
+                 resume_url=data['resume_url'])
---
+++ database1.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine,text
+
+db_connection_string={  create_engine("mysql+pymysql://uk7bg6n6qjx5s9g379h5:pscale_pw_Ftzy7UhC3xGlM13jXrFR0pSOZJhXLcBK2S6wSEaFBx2@aws.connect.psdb.cloud/okaracareer?charset=utf8b4")
+             }
+
+
+
+
+
